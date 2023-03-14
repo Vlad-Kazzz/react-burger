dbb4849a6bb77f17ef807345615ff7ed7ef3986b
Vlad Kazaryan <vlad.kazzz@yandex.ru>
2023-03-15 00:54:33 +0300

    added: burger constructor and ingredients

---
+++ src/ingredient/ingredient.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+    Counter,
+    CurrencyIcon
+  } from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./ingredient.module.css";
+
+const Ingredient = (props) => {
+    const { name, image, price } = props.ingredient;
+    const counter = props.counter;
+    const handleClick = () => props.onClick(props.ingredient)
+    return (
+      <div className={`${styles.ingredient}`} onClick={handleClick} >
+        {counter > 0 ? <Counter count={counter} size="default" /> : null}
+        <img className={`ml-4 mr-4 mb-1`} src={image} alt="ingredient" />
+        <div className={`${styles.priceAndIcon} text_type_digits-default mb-4`}>
+          <span>{price}</span>
+          <CurrencyIcon />
+        </div>
+        <div className={`${styles.ingredientName} text text_type_main-default`}>
+          {name}
+        </div>
+        
+      </div>
+    );
+  };
+  
+
+export default Ingredient
